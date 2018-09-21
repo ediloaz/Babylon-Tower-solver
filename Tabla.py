b6da150b24f557f509cc8c08481d799871524313
Alonso Rivas Solano <alonsors.809@gmail.com>
2018-09-20 22:17:09 -0600

    casi movimiemto arriba

--- Tabla.py
+++ Tabla.py
@@ -142,26 +142,21 @@ class Tabla(object):
         
     #RotarColumnas
             
-    def PosO():
-        i1=5
-        j=5
+    def PosO(self):
         for i in range(Columnas):
-            try:
-                j = (Torre[0].index('O'))
-                i1=i
-            except ValueError:
-                print ('a')
+            for j in range(Filas):
+                if (self.tabla[i][j].getColor()=='O'):
+                    return (i,j)
             
-        print (i1,j)
-
 
     def RotarElOArriba(self, PosicionI, PosicionJ):
-        if (PosicionI == 0 or self.tabla[PosicionI][PosicionJ] != 'O' ):
-            print ('Error')
+        if (PosicionI == 0 ):
+            return 0
         else:
             color = self.tabla[PosicionI][PosicionJ]
             self.tabla[PosicionI][PosicionJ] = self.tabla[PosicionI-1][PosicionJ]
             self.tabla[PosicionI-1][PosicionJ] = color
+            return 1
 
     def RotarElOAbajo(self, PosicionI, PosicionJ):
         if (PosicionI == Columnas-1 or self.tabla[PosicionI][PosicionJ] != 'O' ):
